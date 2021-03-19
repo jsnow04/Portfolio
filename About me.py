8cd5fdffd29e8fc1d7b3e8036620b112ce866b72
jsnow04 <71038859+jsnow04@users.noreply.github.com>
2021-03-19 12:43:40 -0600

    Update About me.py

--- About me.py
+++ About me.py
@@ -1,4 +1,6 @@
-Print(" Hi I am Jaxon Snow and this is my Programer Portfolio",Arial,50)
+print(" Hi I am Jaxon Snow and this is my Programer Portfolio",Arial,50)
 
 
-Print("I am a Student at Tooele CLC and I am")
+print("I am a Student at Tooele CLC and I am in my first year as a python programmer. I have little experiance in both HTML and CSS.")
+print("I hope to go into Software development or game development at the U of U or Nuemont college for computer sciences.")
+print("These are some of my best work that I have done in my class.")
