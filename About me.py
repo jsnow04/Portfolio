fdfa1b0c086f001b24a5467b5b6fb0588a9aa145
jsnow04 <71038859+jsnow04@users.noreply.github.com>
2021-03-19 12:34:11 -0600

    Update About me.py

--- About me.py
+++ About me.py
@@ -1,1 +1,4 @@
-Print(" Hi I am Jaxon Snow and this is my Programer Portfolio")
+Print(" Hi I am Jaxon Snow and this is my Programer Portfolio",Arial,50)
+
+
+Print("I am a Student at Tooele CLC and I am")
